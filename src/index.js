b1f4b519fc343a6e7316c41005334385124365c5
Richard Tinajero <rj.tinajero92@gmail.com>
2020-06-11 21:57:24 -0700

    init commit

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+{/* <a href="https://github.com/rj-tinajero" target="_blank"><i class="fab fa-github"></i></a>
+<a href="https://linkedin.com/in/richard-tinajero" target="_blank"><i class="fab fa-linkedin-in"></i></a> */}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
